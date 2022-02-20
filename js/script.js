0aff7c8baa30dce4e93f4967ed068dc1c47b1965
Andres Sebastian Martinez Chas <sebastianmartinezchas@gmail.com>
2022-02-20 05:17:18 -0300

    mensaje animado

--- js/script.js
+++ js/script.js
@@ -140,7 +140,7 @@ function changeQuantity(e) {
     });
 }
 
-//jquery, aparece un mensaje en el carrito, desaparece y vuelve a aparecer.
+//jquery, aparece un mensaje en la cabecera, desaparece y vuelve a aparecer.
 $(document).ready(function() {
     $("#agradecimiento").slideUp(2000).slideDown(2000).fadeOut(1000).fadeIn(3000);
 
